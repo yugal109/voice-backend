6660aa9fe7787558673f3b79b3247784fd7d26d9
Yugal109 <yugalkhati570@gmail.com>
2021-06-17 17:38:03 +0545

    facebook authentiacation added

--- routes/LoginRoute.js
+++ routes/LoginRoute.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bcrypt = require("bcrypt")
+const jwt=require("jsonwebtoken")
 const { OAuth2Client } = require('google-auth-library');
 const User = require("../models/Users")
 const asyncHandler = require("express-async-handler")
@@ -75,6 +76,38 @@ router.post("/googlelogin", asyncHandler(async (req, res) => {
 
 }))
 
+
+router.post("/facebooklogin", asyncHandler(async (req, res) => {
+    const {name,email,picture} = req.body.data;
+
+    const usr=await User.findOne({email})
+    // if(!usr) res.status(404).send("User with this email doesnot exist.")
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(email+process.env.SECRET_KEY, salt)
+
+    const user=new User({
+        firstname:"firstname",
+        lastname:"lastname",
+        username:name,
+        email,
+        password:hashedPassword,
+        image:picture.data.url,
+        address:"No address",
+        phonenumber:"9800000000",
+    })
+    await user.save()
+    const token=jwt.sign({_id:user._id,isAdmin:user.isAdmin,userType:user.userType},
+        process.env.SECRET_KEY)
+
+    res.send({id: user._id,
+        username: user.username,
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        token})
+
+}))
 
 
 module.exports = router
