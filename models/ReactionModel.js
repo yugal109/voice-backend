88deb9e061c3e6926f3b3ff64e6f64ee90034a97
Yugal109 <yugalkhati570@gmail.com>
2021-07-11 21:57:27 +0545

    new react

--- models/ReactionModel.js
+++ models/ReactionModel.js
@@ -1,23 +1,43 @@
-const mongoose =require("mongoose")
+const mongoose = require("mongoose");
 
+// const reactSchema=new mongoose.Schema({
+//     messageId:{
+//         type:mongoose.Schema.Types.ObjectId,
+//         required:true,
+//     },
+//     userId:{
+//         type:mongoose.Schema.Types.ObjectId,
+//         required:true,
+//         ref:'User'
 
-const reactSchema=new mongoose.Schema({
-    messageId:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:true,
-    },
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref:'User'
+//     },
+//     reacts:{
+//         type:String,
+//         enum:["like","haha","love","angry","sad"],
+//         required:true
+//     }
+// })
 
-    },
-    reacts:{
-        type:String,
-        enum:["like","haha","love","angry","sad"],
-        required:true
-    }
-})
+const reactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  reacts: {
+    type: String,
+    enum: ["like", "haha", "love", "angry", "sad"],
+    required: true,
+  },
+});
 
-const React=mongoose.model("React",reactSchema)
-module.exports=React
+const reactSchema = new mongoose.Schema({
+  messageId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  reactions: [reactionSchema],
+});
+
+const React = mongoose.model("React", reactSchema);
+module.exports = React;
--- sockets/LikeSocket.js
+++ sockets/LikeSocket.js
@@ -1,43 +1,89 @@
 const mongoose = require("mongoose");
 const React = require("../models/ReactionModel");
 
+// function LikeSocket(io) {
+//   io.of("/reactions").on("connection", async (socket) => {
+//     socket.on("join", async ({messageId}) => {
+//       const react = await React.find({ messageId: messageId});
+//       socket.emit("getallreacts", { react:react.length });
+//       socket.join(messageId);
+//     });
+
+//     socket.on("reaction", async ({ messageId, userId, reacts }) => {
+//       let react = await React.findOne({ messageId, userId });
+//       if (react) {
+//         if (react.reacts == reacts) {
+//           await React.deleteOne({ messageId, userId });
+//           const rxt=await React.find({ messageId });
+//           io.of("/reactions").to(messageId).emit("getallreacts", { react:rxt.length });
+//         } else {
+//           react.reacts = reacts;
+//           await react.save();
+//           const rxt=await React.find({ messageId });
+//           io.of("/reactions").to(messageId).emit("getallreacts", { react:rxt.length });
+//         }
+//       } else {
+//         const rct = new React({
+//           messageId,
+//           userId,
+//           reacts,
+//         });
+//         await rct.save();
+//         const react = await React.find({ messageId });
+//         io.of("/reactions").to(messageId).emit("getallreacts", { react:react.length });
+//       }
+//     });
+
+//     socket.on("disconnect", () => {
+//       console.log("Reaction is lost.");
+//     });
+//   });
+// }
+
 function LikeSocket(io) {
   io.of("/reactions").on("connection", async (socket) => {
-    socket.on("join", async ({messageId}) => {
-      const react = await React.find({ messageId: messageId});
-      socket.emit("getallreacts", { react:react.length });
+    console.log("Connected.....");
+    socket.on("join", async ({ messageId }) => {
+      const rm = await React.findOne({ messageId });
+      socket.emit("getallreacts", { react: rm?.reactions.length });
       socket.join(messageId);
     });
-
     socket.on("reaction", async ({ messageId, userId, reacts }) => {
-      let react = await React.findOne({ messageId, userId });
-      if (react) {
-        if (react.reacts == reacts) {
-          await React.deleteOne({ messageId, userId });
-          const rxt=await React.find({ messageId });
-          io.of("/reactions").to(messageId).emit("getallreacts", { react:rxt.length });
+      const react = await React.findOne({ messageId });
+      const rxt = react?.reactions.find(e=>e.userId==userId);
+      if (rxt) {
+        if (rxt.reacts == reacts) {
+        //   Dive.update({ _id: diveId }, { "$pull": { "divers": { "user": userIdToRemove } }}, { safe: true, multi:true }, function(err, obj) {
+        //     //do something smart
+        // });
+          // react.reactions.pull({userId})
+          // await react?.save();
+          //  const RXT = await React.findOne({ messageId });
+          // io.of("/reactions")
+          //   .to(messageId)
+          //   .emit("getallreacts", { react: RXT?.reactions?.length });
         } else {
-          react.reacts = reacts;
-          await react.save();
-          const rxt=await React.find({ messageId });
-          io.of("/reactions").to(messageId).emit("getallreacts", { react:rxt.length });
+          rxt.reacts = reacts;
+          await react?.save();
+          const rm = await React.findOne({ messageId });
+          io.of("/reactions")
+            .to(messageId)
+            .emit("getallreacts", { react: rm?.reactions?.length });
         }
       } else {
-        const rct = new React({
+        const react=new React({
           messageId,
-          userId,
-          reacts,
-        });
-        await rct.save();
-        const react = await React.find({ messageId });
-        io.of("/reactions").to(messageId).emit("getallreacts", { react:react.length });
+          reactions:[{userId,reacts}]
+        })
+        await react.save()
+        io.of("/reactions")
+          .to(messageId)
+          .emit("getallreacts", { react: react?.reactions?.length });
       }
     });
-
     socket.on("disconnect", () => {
       console.log("Reaction is lost.");
     });
   });
 }
-
 module.exports = LikeSocket;
